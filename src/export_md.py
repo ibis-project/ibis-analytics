433d67a8db1136e60cc13d592fec856ef09b0af0
Cody <cody@dkdc.dev>
2023-07-23 18:26:15 -0700

    qs

--- src/export_md.py
+++ src/export_md.py
@@ -8,12 +8,12 @@ from dotenv import load_dotenv
 
 # load env vars
 load_dotenv()
-MD_TOKEN = os.getenv("MOTHERDUCK_TOKEN")
-MD_TOKEN = "dummy_token"
+#MD_TOKEN = os.getenv("MOTHERDUCK_TOKEN")
+#MD_TOKEN = "dummy_token"
 
 # connect to motherduck
-con = duckdb.connect(f"md:?[motherduck_token={MD_TOKEN}?saas_mode=true]")
-# con = ibis.connect(f"duckdb://md:?[motherduck_token={MD_TOKEN}?saas_mode=true]")
+con = duckdb.connect(f"md:")
+#con = ibis.connect(f"duckdb://md:")
 
 # export metrics
 con.sql("DROP DATABASE IF EXISTS metrics CASCADE")
