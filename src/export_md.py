8b7aa9ad5bee9d7293a753a0273fb67728806076
Cody <cody@dkdc.dev>
2023-08-06 17:26:24 -0700

    qs

--- src/export_md.py
+++ src/export_md.py
@@ -12,7 +12,7 @@ log.basicConfig(level=log.INFO)
 
 # config.toml
 config = toml.load("config.toml")["motherduck"]
-eda_config = toml.load("config.toml")["eda"]
+transform_config = toml.load("config.toml")["transform"]
 exclude_tables = config["exclude_tables"] or []
 
 # load env vars
@@ -20,21 +20,21 @@ load_dotenv()
 
 # connect to motherduck
 log.info(f"Export database: {config['database']}")
-log.info(f"EDA database: {eda_config['database']}")
-assert f"md:{config['database']}" != eda_config["database"]
+log.info(f"Transform database: {transform_config['database']}")
+assert f"md:{config['database']}" != transform_config["database"]
 con = ibis.connect(f"duckdb://md:{config['database']}")
-eda_con = ibis.connect(f"duckdb://{eda_config['database']}")
+transform_con = ibis.connect(f"duckdb://{transform_config['database']}")
 
 # supported tables in ci
 ci_tables = config["ci_tables"]
 
 # source tables
-source_tables = eda_con.list_tables()
+source_tables = transform_con.list_tables()
 
 # exclude tables
 if not os.getenv("CI"):
     exclude_tables.extend([t for t in source_tables if t not in ci_tables])
-if not eda_config["ci_enabled"]:
+if not transform_config["ci_enabled"]:
     exclude_tables.extend(["jobs", "workflows", "analysis"])
 
 # overwrite tables
@@ -42,7 +42,7 @@ for t in source_tables:
     if t not in exclude_tables:
         log.info(f"Copying {t}...")
         try:
-            con.create_table(t, eda_con.table(t).to_pyarrow(), overwrite=True)
+            con.create_table(t, transform_con.table(t).to_pyarrow(), overwrite=True)
         except Exception as e:
             log.warning(f"Failed to copy {t}...")
             log.warning(e)
